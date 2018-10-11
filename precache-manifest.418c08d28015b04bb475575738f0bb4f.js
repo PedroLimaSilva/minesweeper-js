d8113696bac620560140c75abb64a185e4f5303b
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2018-10-11 22:06:44 +0100

    Updates

---
+++ precache-manifest.418c08d28015b04bb475575738f0bb4f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "38dd7e19054c6ac82d66cdea8c0e4403",
+    "url": "/minesweeper-js/minesweeper-react/static/media/2.38dd7e19.svg"
+  },
+  {
+    "revision": "da6bf36e1e3234ac9284",
+    "url": "/minesweeper-js/minesweeper-react/static/css/main.426b1638.chunk.css"
+  },
+  {
+    "revision": "8cd7744ebe16a6f17f60",
+    "url": "/minesweeper-js/minesweeper-react/static/js/1.8cd7744e.chunk.js"
+  },
+  {
+    "revision": "cc173b88150400ac00e5",
+    "url": "/minesweeper-js/minesweeper-react/static/js/runtime~main.cc173b88.js"
+  },
+  {
+    "revision": "353a5d80c1b4880e47b4f7cfb62e711c",
+    "url": "/minesweeper-js/minesweeper-react/static/media/3.353a5d80.svg"
+  },
+  {
+    "revision": "da6bf36e1e3234ac9284",
+    "url": "/minesweeper-js/minesweeper-react/static/js/main.da6bf36e.chunk.js"
+  },
+  {
+    "revision": "95768a7bc63f3e82e592ee361903218d",
+    "url": "/minesweeper-js/minesweeper-react/static/media/1.95768a7b.svg"
+  },
+  {
+    "revision": "4f4591e250b8f550c121e1e1dc1f80c8",
+    "url": "/minesweeper-js/minesweeper-react/static/media/mine.4f4591e2.svg"
+  },
+  {
+    "revision": "258898ed1d8015375c8f9cf3e371e542",
+    "url": "/minesweeper-js/minesweeper-react/static/media/4.258898ed.svg"
+  },
+  {
+    "revision": "7bcf64c518d53dbd5c30140dcfdee1a3",
+    "url": "/minesweeper-js/minesweeper-react/static/media/flag.7bcf64c5.svg"
+  },
+  {
+    "revision": "c9c4d8337d500d8f92d489b976f3771a",
+    "url": "/minesweeper-js/minesweeper-react/index.html"
+  }
+];
